abbd962d18d1369fc12731198cec635bdc3a39f2
kaveri1606 <145432676+kaveri1606@users.noreply.github.com>
2024-01-24 21:43:06 +0530

    Add files via upload

---
+++ portfolio.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Portfolio</title>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+
+<link rel="stylesheet" type="text/css" href="s1.css">
+</head>
+<body>
+    <center><h2>My Portfolio</h2></center>
+    <hr>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3" >
+                <div class="card card-body" id="profile-wrapper" style="background-color: lightblue ;">
+                    <center><img src="profile.jpg" class="profile"><br></center>
+                    <center><h2 class="name">Hi I'm Kaveri</h2>
+                    <!---<hr>--->
+                    <h4>Web Developer</h4></center>
+                    <!---<h5>I love doing coding and Research</h5>-->
+                </div>
+            </div>
+            <div class="col-md-9">
+                <div class="card card-body" id="profile-wrapper" style="background-color: lightblue;">
+                    <h3>Carrer Objectives</h3>
+                    <hr>    
+                    <p>I am Currently pursuing Computer Engineering In sinhgad Academy of Engineering. I am eager to apply my academic knowledge and hands-on experience to a dynamic role in IT industry. Seeking a challenging position where I can contribute my techenical skills, problem solving abilities, and passion for innovation. I am committed to staying abreast of emerging technologies, collabrating with a skilled team and continuously learning to drive organizational success. My goal is to embark on a rewarding carrer that allows me to make meaningful contribution while further developing my experties in computer Engineering.<br>
+                    I am dedicated ot being resourceful, innovative and flexible, with a passion for staying abreast of the latest industry trends and technologies. I am excited to apply my academic background and practical experiece to make meaningful contribution to a forward-thinking IT team.</p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card card-body" id="profile-wrapper" style="background-color: lightblue ;">
+                    <h3>Education Details</h3>
+                    <hr>
+                    <h5>Third Year(Computer Engineering pursuing) CGPA:7.00</h5>
+                    <h5>Diploma(Computer Engineering) with 88% in 2022</h5>
+                    <h5>SSC with 86.20% in 2019</h5>
+
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card card-body" id="profile-wrapper" style="background-color: lightblue ;">
+                    <h3>What I Know</h3>
+                    <hr>
+                    <h5>Operating System I know like windows,linux</h5>
+                    <h5>Languages I know like python,Java,HTML,Javascript,C++</h5>
+                    <h5>Databases I Know Like mysql,sql,mongo</h5>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card card-body" id="profile-wrapper" style="background-color: lightblue ;">
+                    <h3>Project Details</h3>
+                    <hr>
+                    <h5>Title: Employee Sales Management Website </h5>
+                    <h5>Organization: K.K. Wagh Polytechnic, Nashik </h5>
+                    <h5>Title: AI Enable Water well prediction</h5>
+                    <h5>Organization: Sinhgad Academy of engineering</h5>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card card-body" id="profile-wrapper" style="background-color: lightblue ;">
+                    <h3>Internship</h3>
+                    <hr>
+                    <h5>I have completed 45 days internship on <b>"Web Development"</b> at "NPIT Solution" from 01/07/2021 to 15/08/2021</h5><br> 
+                    <h5>I am pursuing internship on <b>"Web Development"</b>at "Codsoft"</h5> 
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
